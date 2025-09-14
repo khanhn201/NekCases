19952f67ff5e197d7e295e48e5ae89299181aa3b
Khanh Nguyen <khanh201dth@gmail.com>
2025-09-14 11:57:09 -0500

    update hydrJump

--- hydrJump2D/plot_outflow_height.sh
+++ hydrJump2D/plot_outflow_height.sh
@@ -3,15 +3,17 @@
 # Input file
 LOGFILE="logfile"
 # Extract step and outflow_height values
-paste <(grep "Step" "$LOGFILE" | awk '{print $2}' | tr -d ',') \
-      <(grep "outflow_height" "$LOGFILE" | awk '{print $2}') > outflow_step.dat
+# paste <(grep "Step" "$LOGFILE"  | head- 7000 | awk '{print $2}' | tr -d ',') \
+#       <(grep "outflow_height" | head- 7000 | "$LOGFILE" | awk '{print $2}') > outflow_step.dat
+paste <(grep "Step" "$LOGFILE" | awk '{for(i=1;i<=NF;i++) if($i=="t=") {print $(i+1)}}' | tr -d ',') \
+      <(grep "outflow_height" "$LOGFILE" | awk '{print $2}') | head -n 7000 > outflow.dat
 
 # Display plot
 gnuplot -persist <<EOF
 set terminal qt size 1200,800 enhanced font 'Arial,12'
 set title "Outflow Height Over Time"
-set xlabel "Step"
+set xlabel "t"
 set ylabel "Outflow Height"
 set grid
-plot "outflow_step.dat" using 1:2 with linespoints title "Outflow Height"
+plot "outflow.dat" using 1:2 with linespoints title "Outflow Height"
 EOF
